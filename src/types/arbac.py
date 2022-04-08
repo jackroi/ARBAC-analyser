cfdaab200470c8d9e0c6f6712d5ac94d2ef7c3f3
Giacomo Rosin <giacomorosin@gmail.com>
2022-04-08 10:58:26 +0200

    Improve types/arbac.pyls
    Clean and comment code

--- src/types/arbac.py
+++ src/types/arbac.py
@@ -1,13 +1,31 @@
-"""ARBAC related types"""
+"""ARBAC related types.
+
+This module exports the following classes:
+- `UserToRole`;
+- `UserToRoleAssignment`;
+- `CanAssignRule`;
+- `CanRevokeRule`;
+- `Policy`;
+- `Arbac`;
+- `ArbacReachability`.
+"""
 
 
 from dataclasses import dataclass
-from typing import List
+from typing import List, Set
 
 
-@dataclass
+@dataclass(frozen=True)
 class UserToRole:
-    """UserToRole"""
+    """Association between a user and a role.
+
+    It is an immutable dataclass (so it has an implicit __hash__ method,
+    and can be stored into sets).
+
+    Attributes:
+        user: The user name.
+        role: The role name.
+    """
 
     user: str
     role: str
@@ -15,16 +33,26 @@ class UserToRole:
 
 @dataclass
 class UserToRoleAssignment:
-    """ARBAC user-to-role assignment"""
+    """ARBAC user-to-role assignment.
 
-    # TODO: eventally other formats, such as dict of list
+    Attributes:
+        user_role_list: Set of UserToRole objects.
+    """
 
-    user_role_list: List[UserToRole]
+    user_role_list: Set[UserToRole]
 
 
 @dataclass
 class CanAssignRule:
-    """ARBAC can assign rule"""
+    """ARBAC can assign rule.
+
+    Attributes:
+        admin_role: The admin role (the one that fires the assignment).
+        positive_roles: List of positive roles.
+        negative_roles: List of negative roles.
+        target_role: The target role (the one that gets assigned
+            if preconditions are met).
+    """
 
     admin_role: str
     positive_roles: List[str]
@@ -34,14 +62,25 @@ class CanAssignRule:
 
 @dataclass
 class CanRevokeRule:
-    """ARBAC can revoke rule"""
+    """ARBAC can revoke rule.
+
+    Attributes:
+        admin_role: The admin role (the one that fires the revocation).
+        target_role: The target role (the one that gets revocated).
+    """
+
     admin_role: str
     target_role: str
 
 
 @dataclass
 class Policy:
-    """ARBAC policy"""
+    """ARBAC policy.
+
+    Attributes:
+        can_assign: List of can assign rules.
+        can_revoke: List of can revoke rules.
+    """
 
     can_assign: List[CanAssignRule]
     can_revoke: List[CanRevokeRule]
@@ -49,28 +88,30 @@ class Policy:
 
 @dataclass
 class Arbac:
-    """ARBAC"""
+    """ARBAC.
 
-    # role list
+    Attributes:
+        role_list: List of roles in the ARBAC system.
+        user_list: List of users in the ARBAC system.
+        user_to_role_assignment: The user-to-role assignment.
+        policy: The policy of the ARBAC system
+            (can assign and can revoke rules).
+    """
+
     role_list: List[str]
-
-    # user list
     user_list: List[str]
-
-    # user to role
     user_to_role_assignment: UserToRoleAssignment
-
-    # policy
     policy: Policy
 
 
 @dataclass
 class ArbacReachability:
-    """ARBAC reachability problem"""
+    """ARBAC reachability problem.
 
-    # arbac
+    Attributes:
+        arbac: The ARBAC system.
+        goal: The goal role.
+    """
+
     arbac: Arbac
-
-    # goal
     goal: str
-
